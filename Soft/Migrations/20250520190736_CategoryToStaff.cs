ce9785d7de24d8ac3701d1313049d1694169bcdf
Alesja Aleksandrova <alesja@taltech.ee>
2025-05-20 22:12:42 +0300

    Muudatused _EditAllStaff failis (lisatud Category Id), tehtud migratsioon ja uuendatud DB.

---
+++ Soft/Migrations/20250520190736_CategoryToStaff.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mvc.Soft.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryToStaff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AllCategoriesId",
+                table: "AllStaff",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllCategoriesId",
+                table: "AllStaff");
+        }
+    }
+}
