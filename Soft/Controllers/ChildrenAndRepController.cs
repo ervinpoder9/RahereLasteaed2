e34d6bda5b1fcbc56330f97fb9958569d961a68b
Alesja Aleksandrova <alesja@taltech.ee>
2025-05-21 22:28:26 +0300

    Lisatud ChildrenAndRep kontroller.

---
+++ Soft/Controllers/ChildrenAndRepController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc.Data;
+using Mvc.Domain;
+using Mvc.Facade;
+using Mvc.Soft.Data;
+
+namespace Mvc.Soft.Controllers;
+
+public class ChildrenAndRepController(ApplicationDbContext context) 
+    : BaseController<ChildrenAndRep, ChildrenAndRepData, ChildrenAndRepView>
+    (context, new ChildrenAndRepViewFactory(), d => new ChildrenAndRep(d))
+{ }
+
