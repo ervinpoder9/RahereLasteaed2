88332f178e02675794c67abc17b6ec48f21369ec
Alesja Aleksandrova <alesja@taltech.ee>
2025-05-26 21:21:45 +0300

    _Layout'i lisatud Kindergarten Team.

--- Facade/AllCategoriesView.cs
+++ Facade/AllCategoriesView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,5 @@ namespace Mvc.Facade;
 
 [DisplayName("All Categories")] public sealed class AllCategoriesView : EntityView
 {
-    public string? CategoryName { get; set; }
+    [Display(Name = "Category")] public string? CategoryName { get; set; }
 }
