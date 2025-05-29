90e1367f4419a97102e4a7cf6be8d1ca9c96a292
rando <rakuba@ttu.ee>
2025-05-30 00:38:33 +0300

    GroupViewFactory tests

--- Tests/Domain/GroupTests.cs
+++ Tests/Domain/GroupTests.cs
@@ -1,10 +1,25 @@
+using Mvc.Data;
+using Mvc.Domain;
+
 namespace Mvc.Tests.Domain;
 
-[TestClass]
-public class GroupTests
-{
-    [TestMethod]
-    public void TestMethod1()
-    {
+[TestClass] public class GroupTests : BaseClassTests<Group, Entity<GroupData>> {
+    protected override Group createObj() {
+        var d = new GroupData {
+            Id = 1,
+            Name = "Name",
+            Capacity = DateTime.Today,
+            PrimaryTeacher = "Genre",
+            AssistantTeacher = "Rating",
+            RoomNumber = 100
+        };
+        return new Group(d);
     }
+    [TestMethod] public void TitleTest() => equal("Title", obj?.Title);
+    [TestMethod] public void ReleaseDateTest() => equal(DateTime.Today, obj?.ReleaseDate);
+    [TestMethod] public void GenreTest() => equal("Genre", obj?.Genre);
+    [TestMethod] public void PriceTest() => equal(1.0, obj?.Price);
+    [TestMethod] public void RatingTest() => equal("Rating", obj?.Rating);
+    [TestMethod] public void IdTest() => equal(1, obj?.Id);
+    [TestMethod] public void DataTest() => notNull(obj?.data);
 }
--- Tests/Facade/GroupViewFactoryTests.cs
+++ Tests/Facade/GroupViewFactoryTests.cs
@@ -1,10 +1,63 @@
+using Mvc.Data;
+using Mvc.Facade;
+
 namespace Mvc.Tests.Facade;
 
-[TestClass]
-public class GroupViewFactoryTests
-{
-    [TestMethod]
-    public void TestMethod1()
-    {
+[TestClass] public class GroupViewFactoryTests : SealedTests<GroupViewFactory, AbstractViewFactory<GroupData, GroupView>> {
+    private GroupData? data;
+    private GroupView? view;
+    [TestInitialize] public override void Initialize() {
+        base.Initialize();
+        data = crData();
+        view = crView();
+    }
+    [TestCleanup] public override void Cleanup() {
+        base.Cleanup();
+        data = null;
+        view = null;
+    }
+    private GroupView crView() {
+        var v = new GroupView {
+            Id = 1,
+            Name = "Group",
+            Capacity = 10,
+            PrimaryTeacher = "PTeacher",
+            AssistantTeacher = "ATeacher",
+            RoomNumber = 100
+        };
+        return v;
+    }
+    private GroupData crData() {
+        var d = new GroupData {
+            Id = 1,
+            Name = "Name",
+            Capacity = 10,
+            PrimaryTeacher = "PTeacher",
+            AssistantTeacher = "ATeacher",
+            RoomNumber = 100
+        };
+        return d;
+    }
+    [TestMethod] public void CreateViewTest() {
+        var f = new GroupViewFactory();
+        var v = f.CreateView(data);
+        notNull(v);
+        equal(data?.Id, v.Id);
+        equal(data?.Name, v.Name);
+        equal(data?.Capacity, v.Capacity);
+        equal(data?.PrimaryTeacher, v.PrimaryTeacher);
+        equal(data?.AssistantTeacher, v.AssistantTeacher);
+        equal(data?.RoomNumber, v.RoomNumber);
+    }
+    [TestMethod] public void CreateDataTest() {
+        var f = new GroupViewFactory();
+        var d = f.CreateData(view);
+        notNull(d);
+        equal(view?.Id, d.Id);
+        equal(view?.Name, d.Name);
+        equal(view?.Capacity, d.Capacity);
+        equal(view?.PrimaryTeacher, d.PrimaryTeacher);
+        equal(view?.AssistantTeacher, d.AssistantTeacher);
+        equal(view?.RoomNumber, d.RoomNumber);
     }
 }
