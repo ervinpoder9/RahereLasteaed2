871bcfe056f227e2a5b3672a7c465685c324b74f
elorak <elora@oac.io>
2025-06-01 11:29:57 +0300

    initial view for Absences

--- Facade/AbsenceView.cs
+++ Facade/AbsenceView.cs
@@ -4,8 +4,11 @@ using System.ComponentModel;
 
 namespace Mvc.Facade;
 
-[DisplayName("Absences")]
+[DisplayName("Absence")]
 public sealed class AbsenceView : EntityView {
-
+    internal const string absenceDate = "Absence Date";
+    internal const string submissionDate = "Submission Date";
+    [Display(Name = absenceDate), DataType(DataType.Date)] public DateTime AbsenceDate { get; set; }
+    [Display(Name = submissionDate), DataType(DataType.DateTime)] public DateTime SubmissionDate { get; set; }
 }
 
